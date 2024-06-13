87aad091a703cd6a78c0d71767ac1c716544e2b4
Roan <ikbenroan@gmail.com>
2024-06-13 11:42:13 +0200

    Update setup-kiosk.sh

--- Ubuntu/setup-kiosk.sh
+++ Ubuntu/setup-kiosk.sh
@@ -78,7 +78,21 @@ gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 systemctl stop lightdm.service
 systemctl disable lightdm.service
+systemctl mask suspend.target
 
-# Step 8: Reboot
+# Step 8: Add xset commands to disable DPMS and screen blanking
+echo "Disabling DPMS and screen blanking..."
+cat <<EOF > /etc/xdg/autostart/disable-dpms.desktop
+[Desktop Entry]
+Type=Application
+Exec=sh -c 'xset -display :0.0 -dpms; xset -display :0.0 s off; xset -display :0.0 s noblank'
+Hidden=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=Disable DPMS
+Name=Disable DPMS
+Comment=Disable DPMS and screen blanking
+EOF
+
+# Step 9: Reboot
 echo "Rebooting..."
 reboot
