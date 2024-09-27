29da3ec6cef6eee17d35d644b842336dd15f66c2
Roan de Graaf <ikbenroan@gmail.com>
2024-09-27 12:45:45 +0200

    Update dual-monitor-kiosk.sh

--- Ubuntu/dual-monitor-kiosk.sh
+++ Ubuntu/dual-monitor-kiosk.sh
@@ -2,43 +2,12 @@
 
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
+    echo "Please run as root"
+    exit
 fi
 
 read -p "Enter the URL to display on the first monitor in kiosk mode: " URL1
 read -p "Enter the URL to display on the second monitor in kiosk mode: " URL2
-
-# # Step 1: Modify /etc/fstab to mount root filesystem as read-only
-# echo "Modifying /etc/fstab..."
-# echo "UUID=$(blkid -s UUID -o value /) / ext4 ro 0 1" | tee -a /etc/fstab > /dev/null
-
-# # Step 2: Install overlayroot
-# echo "Installing overlayroot..."
-# apt update
-# apt install -y overlayroot
-
-# # Step 3: Configure overlayroot
-# echo "Configuring overlayroot..."
-
-# cat <<EOF >> /etc/overlayroot.conf
-# overlayroot="tmpfs"
-# overlayroot_cfgdisk="tmpfs"
-# overlayroot_mode="ro"
-# overlayroot_options="sync=always"
-# overlayroot_tmpfs_size="50%"
-# overlayroot_quiet="yes"
-# EOF
-
-# # Step 4: Update bootloader configuration
-# echo "Updating bootloader configuration..."
-# sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 overlayroot=tmpfs"/' /etc/default/grub
-
-# # Change GRUB_TIMEOUT to 0
-# echo "Setting GRUB_TIMEOUT to 0..."
-# sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-
-# update-grub
 
 # Step 5: Install Chromium and configure kiosk mode
 echo "Installing Chromium..."
@@ -46,10 +15,11 @@ apt install -y chromium-browser
 
 echo "Configuring Chromium kiosk mode..."
 mkdir -p /etc/xdg/autostart
+
 cat <<EOF > /etc/xdg/autostart/chromium1.desktop
 [Desktop Entry]
 Type=Application
-Exec=sh -c "DISPLAY=:0 chromium-browser --noerrdialogs --kiosk --window-position=0,0 --window-size=1920,1080 '$URL1'; sleep 5"
+Exec=chromium-browser --noerrdialogs --kiosk --window-position=0,0 --window-size=1920,1080 '$URL1'; sleep 5"
 Hidden=false
 X-GNOME-Autostart-enabled=true
 Name[en_US]=Chromium1
@@ -60,7 +30,7 @@ EOF
 cat <<EOF > /etc/xdg/autostart/chromium2.desktop
 [Desktop Entry]
 Type=Application
-Exec=sh -c "DISPLAY=:0 chromium-browser --noerrdialogs --kiosk --window-position=1920,0 --window-size=1920,1080 '$URL2'"
+Exec=chromium-browser --noerrdialogs --kiosk --window-position=1920,0 --window-size=1920,1080 '$URL2'"
 Hidden=false
 X-GNOME-Autostart-enabled=true
 Name[en_US]=Chromium2
