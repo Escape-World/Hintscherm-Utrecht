12b93b0bef59b8e8959ae3e3d4b8d452fc946a3b
Roan de Graaf <ikbenroan@gmail.com>
2024-09-27 11:19:47 +0200

    update

--- Ubuntu/dual-monitor-kiosk.sh
+++ Ubuntu/dual-monitor-kiosk.sh
@@ -9,36 +9,36 @@ fi
 read -p "Enter the URL to display on the first monitor in kiosk mode: " URL1
 read -p "Enter the URL to display on the second monitor in kiosk mode: " URL2
 
-# Step 1: Modify /etc/fstab to mount root filesystem as read-only
-echo "Modifying /etc/fstab..."
-echo "UUID=$(blkid -s UUID -o value /) / ext4 ro 0 1" | tee -a /etc/fstab > /dev/null
+# # Step 1: Modify /etc/fstab to mount root filesystem as read-only
+# echo "Modifying /etc/fstab..."
+# echo "UUID=$(blkid -s UUID -o value /) / ext4 ro 0 1" | tee -a /etc/fstab > /dev/null
 
-# Step 2: Install overlayroot
-echo "Installing overlayroot..."
-apt update
-apt install -y overlayroot
+# # Step 2: Install overlayroot
+# echo "Installing overlayroot..."
+# apt update
+# apt install -y overlayroot
 
-# Step 3: Configure overlayroot
-echo "Configuring overlayroot..."
+# # Step 3: Configure overlayroot
+# echo "Configuring overlayroot..."
 
-cat <<EOF >> /etc/overlayroot.conf
-overlayroot="tmpfs"
-overlayroot_cfgdisk="tmpfs"
-overlayroot_mode="ro"
-overlayroot_options="sync=always"
-overlayroot_tmpfs_size="50%"
-overlayroot_quiet="yes"
-EOF
+# cat <<EOF >> /etc/overlayroot.conf
+# overlayroot="tmpfs"
+# overlayroot_cfgdisk="tmpfs"
+# overlayroot_mode="ro"
+# overlayroot_options="sync=always"
+# overlayroot_tmpfs_size="50%"
+# overlayroot_quiet="yes"
+# EOF
 
-# Step 4: Update bootloader configuration
-echo "Updating bootloader configuration..."
-sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 overlayroot=tmpfs"/' /etc/default/grub
+# # Step 4: Update bootloader configuration
+# echo "Updating bootloader configuration..."
+# sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 overlayroot=tmpfs"/' /etc/default/grub
 
-# Change GRUB_TIMEOUT to 0
-echo "Setting GRUB_TIMEOUT to 0..."
-sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+# # Change GRUB_TIMEOUT to 0
+# echo "Setting GRUB_TIMEOUT to 0..."
+# sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
 
-update-grub
+# update-grub
 
 # Step 5: Install Chromium and configure kiosk mode
 echo "Installing Chromium..."
